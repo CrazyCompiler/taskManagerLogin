9cdd5d525fff88b5b15b9d1bcef5748951d37e74
VirajParab <parabveer01@gmail.com>
2016-05-06 11:29:15 +0530

    added nginx

--- handlers/handler.go
+++ handlers/handler.go
@@ -8,19 +8,24 @@ import (
 	"taskManagerLogin/tokenGenerator"
 )
 
-const redirectUrl string = "http://localhost:8888/tasks.html"
+const redirectUrl string = "/web/tasks.html"
 
 func Login(context config.Context) http.HandlerFunc{
 	return func(res http.ResponseWriter, req *http.Request) {
 		req.ParseForm()
-
 		id := strings.Join(req.Form["Id"], "")
 		token := tokenGenerator.Generate(id,context)
 
 		expiration := time.Now().Add(365 * 24 * time.Hour)
 
-		tokenCookie := http.Cookie{Name: "taskManagerToken",Value:token,Expires:expiration}
-		idCookie := http.Cookie{Name: "taskManagerId",Value:id,Expires:expiration}
+		tokenCookie := http.Cookie{
+			Name: "taskManagerToken",
+			Value:token,
+			Expires:expiration,
+		}
+
+		idCookie := http.Cookie{
+			Name: "taskManagerId",Value:id,Expires:expiration}
 
 		http.SetCookie(res, &tokenCookie)
 		http.SetCookie(res, &idCookie)
