0f6b5277d384cb9cc707fdae3ca3a53555fa43af
VirajParab <parabveer01@gmail.com>
2016-05-09 16:22:38 +0530

    added logout

--- handlers/handler.go
+++ handlers/handler.go
@@ -7,6 +7,7 @@ import (
 	"time"
 	"taskManagerLogin/tokenGenerator"
 	"os"
+	"strconv"
 )
 
 const redirectUrl string = "/web/tasks.html"
@@ -45,4 +46,29 @@ func GetClientId(res http.ResponseWriter, req *http.Request) {
 	clientId := os.Getenv("googleClientId")
 	res.Write([]byte(clientId))
 	return
+}
+
+
+func Logout(context config.Context) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		expiration := time.Now().AddDate(0, 0, 1)
+		tokenCookie := http.Cookie{
+			Name: "taskManagerToken",
+			Value:strconv.FormatInt(time.Now().Unix(), 10),
+			Expires:expiration,
+			Secure:true,
+		}
+
+		idCookie := http.Cookie{
+			Name: "taskManagerId",
+			Value:strconv.FormatInt(time.Now().Unix(), 10),
+			Expires:expiration,
+			Secure:true,
+		}
+
+		http.SetCookie(res, &tokenCookie)
+		http.SetCookie(res, &idCookie)
+		http.Redirect(res,req,"/",http.StatusTemporaryRedirect)
+		return
+	}
 }
